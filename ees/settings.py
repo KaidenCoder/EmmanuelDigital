1c3774c8216b5593ce8130e2c804a33317f16da8
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-01-24 20:27:04 +0530

    static dir name changed

--- ees/settings.py
+++ ees/settings.py
@@ -163,9 +163,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static_env"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
