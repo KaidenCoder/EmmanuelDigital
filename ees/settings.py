d719ebd24892165df3f1b8e102fc96b6580676bf
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-01-24 14:20:16 +0530

    database rename

--- ees/settings.py
+++ ees/settings.py
@@ -102,7 +102,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'eesdigitalDB;',
+            'NAME': 'eesdigitalDB',
             'USER': 'eehss',
             'PASSWORD': 'testing123',
             'HOST': 'localhost',
