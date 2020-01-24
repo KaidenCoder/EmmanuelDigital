7527d7edffdb53988221a9700c3a2ee7dae8f44e
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2020-01-24 14:08:08 +0530

    Added IP Address

--- ees/settings.py
+++ ees/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cgz10y3m&rzrdrlc8nzod_l!$zcn0n0m841$rxw4*ded3t=169'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['134.209.151.43']
 
 
 # Application definition
